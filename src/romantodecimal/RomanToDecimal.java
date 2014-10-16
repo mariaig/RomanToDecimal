67fe49b6f2a06efda311c33a359169bc102b3343
Maria <Maria@Maria-Laptop>
2014-10-16 18:56:09 +0300

    Task1Brindu

--- src/romantodecimal/RomanToDecimal.java
+++ src/romantodecimal/RomanToDecimal.java
@@ -13,14 +13,15 @@ public class RomanToDecimal {
     //          value is the decimal number 
     private static final HashMap<String, Integer> symbols = new HashMap<String, Integer>() {
         {
-            put("I", 1);
-            put("V", 5);
-            put("X", 10);
-            put("L", 50);
-            put("C", 100);
-            put("D", 500);
             put("M", 1000);
             put("E", 5000);
+            put("D", 500);
+            put("C", 100);
+            put("L", 50);
+            put("X", 10);
+            put("V", 5);
+            put("I", 1);
+
         }
     };
 
@@ -52,37 +53,35 @@ public class RomanToDecimal {
         }
     }
 
-    private static void checkRomanNumber(String number) throws InvalidNumber {
+    private static void checkRomanNumber(String nr) throws InvalidNumber {
         int[] vector = new int[symbols.size()];
-        char currentLetter = number.charAt(0);
-        char nextLetter = number.charAt(1);
+        char currentLetter = nr.charAt(0);
+        char nxtLetter = nr.charAt(1);
 
-        int maxLetter;
-        if (symbols.get(currentLetter + "") < symbols.get(nextLetter + "")) {
-            maxLetter = symbols.get(nextLetter+"");
-            vector[index(nextLetter)] = 1;
+        int maxLeter;
+        if (symbols.get(currentLetter + "") < symbols.get(nxtLetter + "")) {
+            maxLeter = symbols.get(nxtLetter + "");
+            vector[index(nxtLetter)] = 1;
         } else {
-            maxLetter = symbols.get(currentLetter+"");
+            maxLeter = symbols.get(currentLetter + "");
         }
 
-        currentLetter = nextLetter;
-        for (int i = 2; i < number.length(); i++) {
-            nextLetter = number.charAt(i);
-            int max2;
-            if (symbols.get(currentLetter + "") < symbols.get(nextLetter+"")) {
-                max2 = symbols.get(nextLetter+"");
-                if (vector[index(nextLetter)] == 1) {
+        currentLetter = nxtLetter;
+        for (int j = 2; j < nr.length(); j++) {
+            nxtLetter = nr.charAt(j++);
+            int max2 = 0;
+            if (symbols.get(currentLetter + "") < symbols.get(nxtLetter + "")) {
+                max2 = symbols.get(nxtLetter + "");
+                if (vector[index(nxtLetter)] == 1) {
                     throw new InvalidNumber();
                 }
-                vector[index(nextLetter)] = 1;
-            } else {
-                max2 = symbols.get(currentLetter+"");
+                vector[index(nxtLetter)] = 1;
             }
-            if (max2 > maxLetter) {
+            if (max2 > maxLeter) {
                 throw new InvalidNumber();
             }
-            currentLetter = nextLetter;
-            maxLetter = max2;
+            currentLetter = nxtLetter;
+            maxLeter = max2;
         }
 
     }
@@ -91,41 +90,40 @@ public class RomanToDecimal {
      without having an instance of RomanToDecimal class
      */
 
-    public static int parseInput(String romanNumber) throws InvalidCharacterException, InvalidNumber {
+    public static int parseInput(String romanNr) throws InvalidCharacterException, InvalidNumber {
 
-        if (romanNumber.length() == 1) {
+        if (romanNr.length() == 1) {
             //if the length is 1, you just have to return the value
-            checkIfIsValid(romanNumber);
-            return symbols.get(romanNumber);
+            checkIfIsValid(romanNr);
+            return symbols.get(romanNr);
         }
 
         //the roman number will pe processed from right to left so
         //I have to save the previous letter in case of a subtract operation
-        String previousLetter = romanNumber.charAt(romanNumber.length() - 1) + "";
-        checkIfIsValid(previousLetter);
+        String prevLetter = romanNr.charAt(romanNr.length() - 1) + "";
+        checkIfIsValid(prevLetter);
 
-        int decimalNumber = symbols.get(previousLetter + "");
+        int decimalNr = symbols.get(prevLetter + "");
 
-        String currentLetter;
-        int i = romanNumber.length() - 2;
-        while (i >= 0) {
+        String curLetter;
+        int j = romanNr.length() - 1;
+        while (j >= 0) {
 
-            currentLetter = romanNumber.charAt(i) + "";
-            checkIfIsValid(currentLetter);
+            curLetter = romanNr.charAt(j) + "";
+            checkIfIsValid(curLetter);
 
-            if (symbols.get(currentLetter) < symbols.get(previousLetter)) {
+            if (symbols.get(curLetter) < symbols.get(prevLetter)) {
                 //when smaller values precede larger values, 
                 //the smaller values are subtracted from the larger values
-                decimalNumber -= symbols.get(currentLetter);
+                decimalNr -= symbols.get(curLetter);
             } else {
                 //otherwise, they are added to the total
-                decimalNumber += symbols.get(currentLetter);
+                decimalNr += symbols.get(curLetter);
             }
-            previousLetter = romanNumber.charAt(i) + "";
-            i--;
+            prevLetter = romanNr.charAt(j--) + "";
         }
-        checkRomanNumber(romanNumber);
-        return decimalNumber;
+        checkRomanNumber(romanNr);
+        return decimalNr;
     }
 
 }
